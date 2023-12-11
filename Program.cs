088d3bb17b65e413c22438958596349186511da8
Criiscvz <153484277+Criiscvz@users.noreply.github.com>
2023-12-11 01:04:39 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿// Interfaz común para componentes y decoradores
+public interface IComponente
+{
+    void Operacion();
+}
+
+// Componente base
+public class Componente : IComponente
+{
+    public void Operacion()
+    {
+        Console.WriteLine("Operación del componente base");
+    }
+}
+
+// Decorador base
+public abstract class Decorador : IComponente
+{
+    protected IComponente componente;
+
+    public Decorador(IComponente componente)
+    {
+        this.componente = componente;
+    }
+
+    public virtual void Operacion()
+    {
+        componente.Operacion();
+    }
+}
+
+// Decorador con funcionalidad adicional
+public class DecoradorA : Decorador
+{
+    public DecoradorA(IComponente componente) : base(componente)
+    {
+    }
+
+    public override void Operacion()
+    {
+        base.Operacion();
+        Console.WriteLine("Operación adicional del Decorador A");
+    }
+}
+
+// Decorador con otra funcionalidad adicional
+public class DecoradorB : Decorador
+{
+    public DecoradorB(IComponente componente) : base(componente)
+    {
+    }
+
+    public override void Operacion()
+    {
+        base.Operacion();
+        Console.WriteLine("Operación adicional del Decorador B");
+    }
+}
+
+// Uso del patrón decorador
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Crear un componente base
+        IComponente componente = new Componente();
+
+        // Envolver el componente base con decoradores
+        componente = new DecoradorA(componente);
+        componente = new DecoradorB(componente);
+
+        // Llamar a la operación en el componente decorado
+        componente.Operacion();
+    }
+}
